28c6e3e796583f479af0ff0ae2212452a09f3d1b
Ilinats <107075825+Ilinats@users.noreply.github.com>
2023-11-04 14:28:13 +0200

    negative radix sort

--- hw1/negative.c
+++ hw1/negative.c
@@ -1,83 +1,102 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct {
-    int first;
-    int second;
-} Pair;
+int maxDigits(int * arr, int count) {
+    int max = 0;
 
-#define MSB 64
-
-void swap(long a[], int i, int j) {
-    long tmp = a[i];
-    a[i] = a[j];
-    a[j] = tmp;
-}
-
-int isBitSet(long x, int k) {
-    int set = (x & (1L << (k - 1))) != 0;
-
-    // Invert the signed bit so that all positive integers come after negative ones
-    return (k == MSB) != set;
-}
-
-void sort(long a[], int d, int lo, int hi) {
-    if (hi < lo || d < 1) return;
-
-    int left = lo - 1;
-    int right = hi + 1;
-
-    for (int i = left + 1; i < right; ) {
-        if (isBitSet(a[i], d)) {
-            swap(a, i, --right);
-        } else {
-            left++;
-            i++;
+    for(int i = 0; i < count; i++) {
+        int digits = 0;
+        int num = arr[i];
+        while(num > 0) {
+            num /= 10;
+            digits++;
+        }
+        if(digits > max) {
+            max = digits;
         }
     }
-    sort(a, d - 1, lo, left);
-    sort(a, d - 1, right, hi);
+
+    return max;
 }
 
-Pair* twoSum(long a[], long sum, int n) {
-    sort(a, MSB, 0, n - 1);
 
-    int i = 0;
-    int j = n - 1;
 
-    while (i < j) {
-        long t = a[i] + a[j];
-        if (t == sum) {
-            Pair* result = (Pair*)malloc(sizeof(Pair));
-            result->first = i;
-            result->second = j;
-            return result;
-        } else if (t < sum) {
-            i++;
-        } else {
-            j--;
+int getDigit(int num, int position) {
+    for (int i = 0; i < position - 1; i++) {
+        num /= 10;
+    }
+    return num % 10;
+}
+
+void radixSortRec(int *arr, int position, int count) {
+    if(position == 0 || count == 0) {
+        return;
+    }
+
+    int buckets[10][10] = {0};
+    int bucketCounts[10] = {0};
+
+    for(int i = 0; i < count; i++) {
+        int digit = getDigit(arr[i], position);
+        buckets[digit][bucketCounts[digit]] = arr[i];
+        bucketCounts[digit]++;
+    }
+
+    for(int i = 0; i < 10; i++) {
+        if(bucketCounts[i])
+            radixSortRec(buckets[i], position - 1, bucketCounts[i]);
+    }
+
+    int index = 0;
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < bucketCounts[i]; j++) {
+            arr[index] = buckets[i][j];
+            index++;
         }
     }
-    return NULL;
+}
+
+void radix_sort(int* arr, unsigned int count) {
+    if(count == 0) {
+        return;
+    }
+
+    int negative[10] = {0};
+    int positive[10] = {0};
+    int negCount = 0;
+    int posCount = 0;
+
+    for(int i = 0; i < count; i++) {
+        if(arr[i] < 0) {
+            negative[negCount] = arr[i];
+            negCount++;
+        } else {
+            positive[posCount] = arr[i];
+            posCount++;
+        }
+    }
+
+    radixSortRec(negative, maxDigits(negative, negCount), negCount);
+    radixSortRec(positive, maxDigits(positive, posCount), posCount);
+
+    for(int i = 0; i < negCount; i++) {
+        arr[i] = negative[negCount - i - 1];
+    }
+
+    for(int i = 0; i < posCount; i++) {
+        arr[i + negCount] = positive[i];
+    }
 }
 
 int main() {
-    long a[] = {3, -5, 2, 8, -7, 1};
-    int n = sizeof(a) / sizeof(a[0]);
-    long sum = 10;
 
-    Pair* result = twoSum(a, sum, n);
+    int arr[9] = {5, -23, 45, 43, -10, 22, -55, 155, -125};
+    printf("%d\n", maxDigits(arr, 9));
 
-    if (result != NULL) {
-        printf("Indices: %d, %d\n", result->first, result->second);
-        free(result);
-    } else {
-        printf("No such pair found\n");
-    }
+    radix_sort(arr, 9);
 
-    for(int i = 0; i < n; i++) {
-        printf("%ld ", a[i]);
-    }
+    for(int i = 0; i < 9; i++) {
+        printf("%d ", arr[i]);
+    };
 
     return 0;
 }
