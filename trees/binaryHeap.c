84d00f050090bd20ce8e6d82f463b3793c807be1
Ilinats <107075825+Ilinats@users.noreply.github.com>
2023-12-01 19:23:19 +0200

    arr into binary heap

---
+++ trees/binaryHeap.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 16
+
+typedef struct BinaryHeap {
+    int *arr;
+    int size;
+} BinaryHeap;
+
+BinaryHeap *createHeap() {
+    BinaryHeap *tmp = (BinaryHeap *)malloc(sizeof(BinaryHeap));
+    tmp->arr = NULL;
+    tmp->size = 0;
+    return tmp;
+}
+
+void printHeap(BinaryHeap *heap) {
+    for(int i = 0; i < heap->size; i++)
+        printf("%d ", heap->arr[i]);
+
+    printf("\n");
+}
+
+BinaryHeap *heapify(int *arr) {
+    BinaryHeap *heap = createHeap();
+    heap->arr = arr;
+    heap->size = SIZE;
+
+    for(int i = SIZE / 2 - 1; i >= 0; i--) {
+        int temp = i;
+
+        while(2 * temp + 1 < SIZE) {
+            int child1 = 2 * temp + 1;
+
+            if(child1 + 1 < SIZE && arr[child1 + 1] > arr[child1])
+                child1++;
+
+            if(arr[temp] < arr[child1]) {
+                int tmp = arr[temp];
+                arr[temp] = arr[child1];
+                arr[child1] = tmp;
+                temp = child1;
+            } else
+                break;
+        }
+    }
+
+    printHeap(heap);
+
+    return heap;
+};
+
+int main() {
+    int arr[SIZE] = {3, 18, 39, 5, 6, 33, 90, 23487, 1234, 12352, 800,  700, 500, 400, 900, 1909000};
+
+    BinaryHeap *heap = heapify(arr);
+    
+    return 0;
+}
