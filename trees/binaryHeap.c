37ae686242605e49a04df3b9639126d735bbad88
Ilinats <107075825+Ilinats@users.noreply.github.com>
2023-12-02 11:24:10 +0200

    sift down update

--- trees/binaryHeap.c
+++ trees/binaryHeap.c
@@ -37,32 +37,30 @@ void printHeap(BinaryHeap *heap) {
     printf("\n");
 }
 
-BinaryHeap *heapify(int *arr) {
+BinaryHeap *heapify(int *arr, int size) {
     BinaryHeap *heap = createHeap();
 
-    for(int i = SIZE / 2 - 1; i >= 0; i--) {
-        int temp = i;
+    for(int i = size / 2 - 1; i >= 0; i--) {
+        int temp = i; // индекс на родителя
 
-        while(2 * temp + 1 < SIZE) {
+        while(2 * temp + 1 < size) {
             int child1 = 2 * temp + 1;
 
-            if(child1 + 1 < SIZE && arr[child1 + 1] > arr[child1])
+            if(child1 + 1 < size && arr[child1 + 1] > arr[child1])  // проверка дали има второ дете и дали е по-голямо от първото
                 child1++;
 
-            if(arr[temp] < arr[child1]) {
+            if(arr[temp] < arr[child1]) {   // проверка дали родителят е по-малък от по-голямото дете и ако е разменяме стойностите им
                 int tmp = arr[temp];
                 arr[temp] = arr[child1];
                 arr[child1] = tmp;
-                temp = child1;
+                temp = child1; // преместване на индекса на родителя на по-голямото дете
             } else
                 break;
         }
     }
 
-    for(int i = 0; i < SIZE; i++)
+    for(int i = 0; i < size; i++)
         push_back(heap, arr[i]);
-
-    printHeap(heap);
 
     return heap;
 };
@@ -70,32 +68,17 @@ BinaryHeap *heapify(int *arr) {
 void siftDown(BinaryHeap *heap, int index, int size) {
     int temp = heap->arr[index];
     heap->arr[index] = heap->arr[0];
-    heap->arr[0] = temp;
+    heap->arr[0] = temp; // размяна на стойностите на първия елемент с последния
 
-    int tempIndex = 0;
-
-    while(2 * tempIndex + 1 < size) {
-        int child1 = 2 * tempIndex + 1;
-
-        if(child1 + 1 < size && heap->arr[child1 + 1] > heap->arr[child1])
-            child1++;
-
-        if(heap->arr[tempIndex] < heap->arr[child1]) {
-            int tmp = heap->arr[tempIndex];
-            heap->arr[tempIndex] = heap->arr[child1];
-            heap->arr[child1] = tmp;
-            tempIndex = child1;
-        } else
-            break;
-    }
+    heap = heapify(heap->arr, size); // пренареждане на масива до heap
 }
 
 BinaryHeap *heapSort(int *arr, int count) {
-    BinaryHeap *heap = heapify(arr);
+    BinaryHeap *heap = heapify(arr, SIZE);
     int size = heap->size;
 
     for(int i = count - 1; i > 0; i--){
-        siftDown(heap, i, --size);
+        siftDown(heap, i, --size); // смяна на последния незамразен елемент с първия и пренареждане на масива до heap
         printHeap(heap);
     }
 
@@ -106,7 +89,6 @@ int main() {
     int arr[SIZE] = {3, 18, 39, 5, 6, 33, 90, 23487, 1234, 12352, 800,  700, 500, 400, 900, 1909000};
 
     BinaryHeap *heap = heapSort(arr, SIZE);
-    printHeap(heap);
 
     return 0;
 }
