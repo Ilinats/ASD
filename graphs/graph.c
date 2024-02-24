4437ee389964053a5282ac1226fad054dfb38b7d
Ilinats <107075825+Ilinats@users.noreply.github.com>
2024-02-24 18:00:39 +0200

    bfs

--- graphs/graph.c
+++ graphs/graph.c
@@ -1,21 +1,19 @@
 /*stepen na vruh - kolko rebra vlizat v nego*/
 #include <stdio.h>
 #include <stdlib.h>
+#include "dlist.h"
 
-typedef struct Node
-{
+typedef struct Node {
     int val;
     struct Node *next;
 } Node;
 
-typedef struct Graph
-{
+typedef struct Graph {
     int numVertices;
     Node **adjList;
 } Graph;
 
-Node *init_node(int val)
-{
+Node *init_node(int val) {
     Node *node = (Node *)malloc(sizeof(Node));
     node->val = val;
     node->next = NULL;
@@ -23,8 +21,7 @@ Node *init_node(int val)
     return node;
 }
 
-Graph *init_graph(int numVertices)
-{
+Graph *init_graph(int numVertices) {
     Graph *graph = (Graph *)malloc(sizeof(Graph));
     graph->numVertices = numVertices;
     graph->adjList = (Node **)calloc(numVertices, sizeof(Node *));
@@ -34,8 +31,7 @@ Graph *init_graph(int numVertices)
 
 void addEdgeDirectional(Graph *graph, int from, int to)
 {
-    if (from < 0 || from >= graph->numVertices || to < 0 || to >= graph->numVertices)
-    {
+    if (from < 0 || from >= graph->numVertices || to < 0 || to >= graph->numVertices) {
         printf("Invalid from or to\n");
         return;
     }
@@ -46,20 +42,17 @@ void addEdgeDirectional(Graph *graph, int from, int to)
     graph->adjList[from] = newNode;
 }
 
-void addEdge(Graph *graph, int from, int to)
-{
+void addEdge(Graph *graph, int from, int to) {
     addEdgeDirectional(graph, from, to);
     addEdgeDirectional(graph, to, from);
 }
 
 void printGraph(Graph *graph)
 {
-    for (int i = 0; i < graph->numVertices; i++)
-    {
+    for (int i = 0; i < graph->numVertices; i++) {
         Node *current = graph->adjList[i];
         printf("Neighbours of %d:\n", i);
-        while (current != NULL)
-        {
+        while (current != NULL) {
             printf("%d ", current->val);
             current = current->next;
         }
@@ -68,7 +61,7 @@ void printGraph(Graph *graph)
 }
 
 void BFS(Graph* G) {
-    Dlist *queue = initDlist();
+    DList *queue = init_dlist();
     insertBegin(queue, 0);
 
     int* visited = (int*)calloc(G->numVertices, G->numVertices * sizeof(int));
@@ -91,12 +84,12 @@ void BFS(Graph* G) {
 }
 
 void BF2S(Graph* G) {
-    Dlist *queue = initDlist();
+    DList *queue = init_dlist();
     insertBegin(queue, 0);
 
     int* visited = (int*)calloc(G->numVertices, G->numVertices * sizeof(int));
-    int parent = (int*)calloc(G->numVertices, G->numVertices * sizeof(int));
-    int dist = (int*)calloc(G->numVertices, G->numVertices * sizeof(int));
+    int* parent = (int*)calloc(G->numVertices, G->numVertices * sizeof(int));
+    int* dist = (int*)calloc(G->numVertices, G->numVertices * sizeof(int));
     visited[0] = 1;
 
     while(queue->head) {
@@ -116,22 +109,32 @@ void BF2S(Graph* G) {
         }
     }
 
-    for(int i = 0; i < G->numVertices; i++) {
-        printf("Parent of %d: %d at dist %d\n", i, parent[i], dist[i]);
-    }
+    for(int i = 0; i < G->numVertices; i++)
+        printf("\nParent of %d: %d at dist %d", i, parent[i], dist[i]);
+
 }
 
-int main()
-{
-    Graph *graph = init_graph(4);
-    addEdge(graph, 1, 2);
-    addEdgeDirectional(graph, 2, 4);
-    addEdgeDirectional(graph, 3, 4);
-    addEdgeDirectional(graph, 2, 3);
+int main() {
+    Graph *graph = init_graph(6);
+    addEdge(graph, 0, 1);
     addEdgeDirectional(graph, 1, 3);
-    addEdgeDirectional(graph, 1, 1);
+    addEdgeDirectional(graph, 2, 3);
+    addEdgeDirectional(graph, 1, 2);
+    addEdgeDirectional(graph, 0, 2);
+    addEdgeDirectional(graph, 0, 0);
+    addEdge(graph, 3, 4);
+    addEdgeDirectional(graph, 2, 4);
+    addEdgeDirectional(graph, 4, 0);
+    addEdgeDirectional(graph, 5, 4);
+    addEdgeDirectional(graph, 5, 1);
+    addEdgeDirectional(graph, 3, 5);
+    addEdgeDirectional(graph, 3, 0);
 
-    printGraph(graph);
+    // printGraph(graph);
+    // printf("\n");
+    // BFS(graph);
+    // printf("\n");
+    BF2S(graph);
 
     return 0;
 }
