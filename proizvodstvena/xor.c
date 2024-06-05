c8959d50122fae97ba9da59299118692cff6ce6f
Ilinats <107075825+Ilinats@users.noreply.github.com>
2024-06-05 19:40:42 +0300

    xor cipher impl.

---
+++ proizvodstvena/xor.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "xor.h"
+
+void getKey(char* keyString, const char* keyName, int keySize) {
+    FILE* key = fopen(keyName, "r");
+
+    if (key == NULL) {
+        printf("Error opening file\n");
+        exit(1);
+    }
+
+    fgets(keyString, keySize, key);
+}
+
+void fileXOR(const char* readName, const char* writeName, char key[100]) {
+    char c;
+    int keyLength = strlen(key);
+    int keyIndex = 0;
+
+    FILE* read = fopen(readName, "rb");
+    FILE* write = fopen(writeName, "wb");
+
+    if(read == NULL || write == NULL) {
+        printf("Error opening file\n");
+        exit(1);
+    }
+
+    while ((c = fgetc(read)) != EOF) {
+        c ^= key[keyIndex];
+        keyIndex = (keyIndex + 1) % keyLength;
+        fputc(c, write);
+    }
+
+    fclose(read);
+    fclose(write);
+}
+
+// int main() {
+//     char keyString[100];
+//     getKey(keyString, "key.txt");
+//     fileXOR("deck.txt", "encrypted.txt", keyString);
+//     fileXOR("encrypted.txt", "decrypted.txt", keyString);
+
+//     return 0;
+// }
