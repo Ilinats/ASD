6d5193ad4319c79e09aa3df5799aa4e5c58a0810
Ilinats <107075825+Ilinats@users.noreply.github.com>
2024-06-15 11:19:21 +0300

    header

--- proizvodstvena/xor.h
+++ proizvodstvena/xor.h
@@ -2,6 +2,8 @@
 #define XOR_H
 
 void getKey(char* keyString, const char* keyName, int keySize);
-void fileXOR(const char* readName, const char* writeName, char key[100]);
+void fileXOR(const char* readName, const char* writeName, const char* key, const char* sizeFileName);
+void fileXORWithSizes(const char* readName, const char* writeName, const char* key, const char* sizeFileName);
+void mixKey(const char* originalKey, char* mixedKey, int keyLength);
 
 #endif
